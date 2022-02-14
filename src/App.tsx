ff028c1e5c59980e05e97125b12ac964448cdd59
hihyejk <hihye.j.kim@gmail.com>
2022-02-14 22:04:03 +0900

    week 11 assignment #8

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import TodoApp from "./containers/TodoApp";
+
+function App() {
+  return <TodoApp />;
+}
+
+export default App;
